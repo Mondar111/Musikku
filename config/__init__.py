2714ada26e6c52ec5aea84f0b19f249eb08788ef
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2022-09-23 15:55:57 +0700

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
 #
-# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# This file is part of < https://github.com/Mondar111/Musikku > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+# Please see < https://github.com/Mondar111/Musikku/blob/master/LICENSE >
 #
 # All rights reserved.
 #
